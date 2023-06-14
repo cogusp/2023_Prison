49b6b022124746ec4593530866f03041ff3d2019
cogusp <84373005+cogusp@users.noreply.github.com>
2023-06-14 18:34:22 +0900

    ..

---
+++ Assets/Deep_Sea/AssetStore/Saltwater fish/scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+ 
+public class PlayerHealth : MonoBehaviour
+{
+    public Image healthBar;
+    public float maxHealth = 100f;
+    public static float health;
+    public GameObject deathCanvas;
+     
+    void Start ()
+    {
+        health = maxHealth;
+    }
+    private void Update() 
+    {
+        if(health < 1)
+        {
+           this.GetComponent<FlyCamera>().enabled = false;
+           deathCanvas.SetActive(true);
+        }
+    }
+ 
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        healthBar.fillAmount = health/maxHealth;
+    }
+}
